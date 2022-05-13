77043c4a4218efc5dcd7d9c29ccece6783c80c8d
Nathan Cai <wenjuncai97@gmail.com>
2022-05-12 23:11:28 -0400

    adding files

--- api/server.js
+++ api/server.js
@@ -1,24 +1,39 @@
-// const express = require('express')
-// const Model = require('./model')
-// const server = express();
+const express = require('express')
+const cors = require('cors')
+const Model = require('./model')
+const server = express();
+server.use(cors())
 
-// server.use(express.json());
+server.use(express.json());
 
-// server.get('/', (req, res) => {
-//     res.send('<h1>BLOOMTECH HEROKU DEPLOYMENT</h1>')
-// })
+server.get('/', (req, res) => {
+    res.send('<h1>BLOOMTECH HEROKU DEPLOYMENT</h1>')
+})
 
-// server.get('/users', (req, res) => {
-//     Model.getUsers()
-//         .then(result => {
-//             res.json(result)
-//         })
-// })
+server.get('/users', (req, res) => {
+    Model.getUsers()
+        .then(result => {
+            res.json(result)
+        })
+})
 
-// server.post('/api/register', (req, res) => {
-//     Model.register()
-// })
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'api is working'})
+})
 
-// server.post('/api/login', (req, res) => {
-//     Model.login()
-// })
+server.post('/api/register', (req, res) => {
+    Model.register()
+})
+
+server.post('/api/login', (req, res) => {
+    Model.login()
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = server;
